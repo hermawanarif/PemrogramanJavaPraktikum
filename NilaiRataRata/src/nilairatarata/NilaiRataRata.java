c655db8d1d715346cf93655db776245d9da5ab1e
Axce29 <Axce29@DESKTOP-BMJEUCT>
2018-10-06 20:28:50 +0700

    Java mencari nilai rata-rata

---
+++ NilaiRataRata/src/nilairatarata/NilaiRataRata.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nilairatarata;
+
+/**
+ *
+ * @author Axce29
+ */
+public class NilaiRataRata {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    int num1 = 10, num2 = 20, num3 = 45;
+    int rata;
+    rata = (num1 + num2 + num3)/3;
+    
+        System.out.println(" number 1 = " + num1);
+        System.out.println(" number 2 = " + num2);
+        System.out.println(" number 3 = " + num3);
+        System.out.println(" Rata - rata = " + rata);
+    }
+    
+}
